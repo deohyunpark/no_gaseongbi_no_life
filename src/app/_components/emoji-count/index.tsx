481d8950c9ae5035e288c6e33a18223229b28e9d
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-08-29 02:29:20 +0900

    Update index.tsx

--- src/app/_components/emoji-count/index.tsx
+++ src/app/_components/emoji-count/index.tsx
@@ -1,32 +1,17 @@
-import { getEmojisCount } from "@/server/get-emojis-count";
 import { Suspense } from "react";
 
-interface CountDisplayProps {
-  count?: number;
-}
-
-function CountDisplay({ count }: CountDisplayProps) {
+function CountDisplay() {
   return (
     <p className="text-gray-500 mb-12 text-base animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
-      {count || "–––"} emojis generated and counting!
+      no 가성비 no life!
     </p>
   );
-}
-
-async function AsyncEmojiCount() {
-  try {
-    const count = await getEmojisCount();
-    return <CountDisplay count={count} />;
-  } catch (error) {
-    console.error("Failed to fetch emoji count:", error);
-    return <CountDisplay />; // 에러 발생 시 기본 메시지 표시
-  }
 }
 
 export function EmojiCount() {
   return (
     <Suspense fallback={<CountDisplay />}>
-      <AsyncEmojiCount />
+      <CountDisplay />
     </Suspense>
   );
 }
