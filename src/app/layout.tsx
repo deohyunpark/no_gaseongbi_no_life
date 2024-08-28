625a33bb0d4e81080aede002c26acbe0a9e06ea5
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-08-29 01:53:09 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,7 +79,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </div>
           {children}
         </main>
-        <Providers />
       </body>
     </html>
   );
