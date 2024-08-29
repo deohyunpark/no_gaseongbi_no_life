ef3a352d2fcc73e9825c1167eb2b5ada3dbfd1c0
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-08-29 18:38:47 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import { APP_STORE_URL, PROD_URL } from "@/lib/constants";
 import { cn } from "@/lib/utils";
-import { Github } from "lucide-react";
+import { Github, Plus } from "lucide-react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { Providers } from "./_components/providers";
 import "./globals.css";
 
@@ -12,13 +13,11 @@ export const dynamic = "force-dynamic";
 export const runtime = "edge";
 
 const BODY_PADDING = "px-4 sm:px-6";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export function generateMetadata(): Metadata {
   const title = "no gaseongbi no life : 노노가성비";
   const description = "배송비딜, 100원딜, 본품체험 등 가성비 넘치는 체험딜을 공유하는 곳";
-
   return {
     metadataBase: new URL(PROD_URL),
     title,
@@ -49,10 +48,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={cn(inter.className, "antialiased bg-gray-100")}>
         <header className={cn("top-0 sticky z-20 w-full py-3 bg-gray-100 flex flex-row flex-nowrap justify-between max-w-5xl mx-auto h-14 items-stretch")}>
-          <Link className="text-black text-lg font-medium flex items-center" href="/">
-            <span>노노가성비</span>
-          </Link>
-
+          <div className="flex items-center space-x-4">
+            <Link className="text-black text-lg font-medium flex items-center" href="/">
+              <span>노노가성비</span>
+            </Link>
+            <Link href="/new-deal">
+              <Button variant="outline" size="sm" className="flex items-center">
+                <Plus className="mr-2 h-4 w-4" /> 새로운 딜 등록하기
+              </Button>
+            </Link>
+          </div>
           <div className="flex flex-row flex-nowrap gap-x-1.5 items-center">
             <Link href="/app?referrer=website" target="_blank" rel="noopener noreferrer">
               <Image src="/_static/AppStoreBadge.svg" alt="App Store Badge" width={120} height={40} priority className="h-8" />
@@ -78,7 +83,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               <span className="mt-2">썸네일 3 설명</span>
             </Link>
           </div>
-          
         </main>
       </body>
     </html>
