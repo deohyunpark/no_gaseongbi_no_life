3bd119ce3c96cc6b15e8ded03ac1fa352b9e7d10
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-08-30 18:16:43 +0900

    Update icon.tsx

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,5 +1,4 @@
 import { FEATURED_OG_IMAGES } from "@/lib/constants"
-import { getRandomItem } from "@/lib/utils"
 import { Favicon } from "./_components/favicon"
 
 export const runtime = "edge"
@@ -7,5 +6,5 @@ export const runtime = "edge"
 export { contentType, size } from "./_components/favicon"
 
 export default async function Icon() {
-  return Favicon({ url: getRandomItem(FEATURED_OG_IMAGES) })
+  const specifiedImageUrl = "https://deohyunpark.github.io/image/profile/%ED%8C%8C%EB%B9%84%EC%BD%98.png"; 
 }
