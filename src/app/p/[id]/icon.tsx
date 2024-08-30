6f5f8c47e540823c878b101210bc67e67c9e08da
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-08-30 20:22:16 +0900

    Update icon.tsx

--- src/app/p/[id]/icon.tsx
+++ src/app/p/[id]/icon.tsx
@@ -1,6 +1,7 @@
 import { Favicon } from "@/app/_components/favicon"
+import { EmojiContextProps } from "@/server/utils"
 
-export default async function Icon({ params }) {
+export default async function Icon({ params }: EmojiContextProps) {
   // 요청 처리 로직 제거
   return null
 }
