40bc90bdf19679eaab2580159a439e1aea837c4d
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-08-30 20:43:07 +0900

    Update route.ts

--- src/app/api/emojis/[id]/route.ts
+++ src/app/api/emojis/[id]/route.ts
@@ -1,21 +1,10 @@
-import { prisma } from "@/server/db"
-import { EmojiContextProps, Response } from "@/server/utils"
 import { NextResponse } from "next/server"
 
 export const runtime = "edge"
 export const fetchCache = "force-no-store"
 export const revalidate = 0
 
-export async function GET(request: Request, { params }: EmojiContextProps) {
-  try {
-    const emoji = await prisma.emoji.findUnique({
-      where: { id: params.id },
-    })
-    if (!emoji) return Response.emojiNotFound()
-
-    return NextResponse.json({ emoji }, { status: 200 })
-  } catch (error) {
-    console.error(error)
-    return Response.internalServerError()
-  }
+export async function GET(request: Request) {
+  // 기본 응답 반환
+  return NextResponse.json({ message: "Hello, World!" }, { status: 200 })
 }
