d0ddd1d4edc01c077f33e39ba10a8109466cb0c8
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-08-30 23:21:14 +0900

    Update supabaseClient.ts

--- src/app/supabaseClient.ts
+++ src/app/supabaseClient.ts
@@ -1,6 +1,18 @@
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = process.env.SUPABASE_URL!;
+// const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || '';
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL 또는 Anon Key가 설정되지 않았습니다.');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
