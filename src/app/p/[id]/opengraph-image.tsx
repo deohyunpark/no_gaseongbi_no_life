add31749a71f60f86ca2e41ba585129b7f6a490f
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-08-30 20:24:25 +0900

    Update opengraph-image.tsx

--- src/app/p/[id]/opengraph-image.tsx
+++ src/app/p/[id]/opengraph-image.tsx
@@ -7,7 +7,7 @@ export { contentType, size } from "@/app/_components/favicon"
 
 export default async function Image({ params }: EmojiContextProps) {
   const data = await getEmoji(params.id)
-  if (!data) return
+  if (!data) return null
 
   const image = data.noBackgroundUrl || data.originalUrl || DEFAULT_OG_IMAGE
   return OpenGraphImage({ url: image })
