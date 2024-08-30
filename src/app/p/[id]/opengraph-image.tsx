c36172c2ac1a1adb012fecc08cb7995e964a222f
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-08-30 20:41:08 +0900

    Update opengraph-image.tsx

--- src/app/p/[id]/opengraph-image.tsx
+++ src/app/p/[id]/opengraph-image.tsx
@@ -1,19 +1,10 @@
 import { OpenGraphImage } from "@/app/_components/opengraph-image"
 import { DEFAULT_OG_IMAGE } from "@/lib/constants"
-import { getEmoji } from "@/server/get-emoji"
 import { EmojiContextProps } from "@/server/utils"
 
 export { contentType, size } from "@/app/_components/favicon"
 
 export default async function Image({ params }: EmojiContextProps) {
-  try {
-    const data = await getEmoji(params.id)
-    if (!data) return OpenGraphImage({ url: DEFAULT_OG_IMAGE })
-
-    const image = data.noBackgroundUrl || data.originalUrl || DEFAULT_OG_IMAGE
-    return OpenGraphImage({ url: image })
-  } catch (error) {
-    console.error("Error fetching emoji data:", error)
-    return OpenGraphImage({ url: DEFAULT_OG_IMAGE })
-  }
+  // 기본 이미지 반환
+  return OpenGraphImage({ url: DEFAULT_OG_IMAGE })
 }
