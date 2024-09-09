13ed96354db7e16316d2c5aa75d7169c16a436cf
deohyunpark <120771212+deohyunpark@users.noreply.github.com>
2024-09-09 15:32:03 +0900

    Update route.ts

--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -1,18 +1,19 @@
 import { NextResponse } from 'next/server';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import cheerio from 'cheerio';
 
 async function fetchData(url: string, retries: number = 3): Promise<string> {
   try {
     const { data } = await axios.get(url);
     return data;
-  } catch (error) {
-    if (retries > 0 && error.response?.status === 429) {
-      await new Promise(resolve => setTimeout(resolve, 2000)); // 2초 지연 후 재시도
-      return fetchData(url, retries - 1);
-    } else {
-      throw error;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 429 && retries > 0) {
+        await new Promise(resolve => setTimeout(resolve, 2000)); // 2초 지연 후 재시도
+        return fetchData(url, retries - 1);
+      }
     }
+    throw error; // AxiosError가 아닌 경우 또는 재시도 후에도 실패한 경우 에러를 다시 던짐
   }
 }
 
@@ -37,7 +38,7 @@ export async function GET(request: Request) {
       imageUrl: imageUrl || '이미지를 찾을 수 없습니다.'
     });
   } catch (error) {
-    console.error('Error fetching data:', error.message);
+    console.error('Error fetching data:', (error as Error).message);
     return NextResponse.json({ error: '웹 페이지에서 데이터를 가져오는 중 오류가 발생했습니다.' }, { status: 500 });
   }
 }
